ba376aacb532c5cb5e70bae8618a4a8bcecaa4e3
Brendon.btd <brendontafadzwa19@gmail.com>
2025-11-15 14:50:49 +0200

    added a products content js file

---
+++ src/productsContent.js
@@ -0,0 +1,54 @@
+const products = [
+    {
+        id: 1,
+        title: "Iphone",
+        price: 244.99,
+        category: "Phones",
+        description: "the best ever yee",
+        image: ""
+    },
+     {
+        id: 2,
+        title: "Iphone",
+        price: 244.99,
+        category: "Phones",
+        description: "the best ever yee",
+        image: ""
+    },
+     {
+        id: 3,
+        title: "Iphone",
+        price: 244.99,
+        category: "Phones",
+        description: "the best ever yee",
+        image: ""
+    },
+     {
+        id: 4,
+        title: "Iphone",
+        price: 244.99,
+        category: "Phones",
+        description: "the best ever yee",
+        image: ""
+    },
+     {
+        id: 5,
+        title: "Iphone",
+        price: 244.99,
+        category: "Phones",
+        description: "the best ever yee",
+        image: ""
+    },
+     {
+        id: 6,
+        title: "Iphone",
+        price: 244.99,
+        category: "Phones",
+        description: "the best ever yee",
+        image: ""
+    },
+];
+
+
+export default products
+
