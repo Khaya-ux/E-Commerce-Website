3439c9e09027c214a6513ffca41579826ef89af1
Khaya-ux <kdube2912@gmail.com>
2025-11-17 18:46:03 +0200

    Set up routing with React Router in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CartPage from "./components/CartPage";
+import CheckoutForm from "./components/CheckoutForm";
+import ProductList from "./components/ProductList";
+import ProductDetail from "./pages/ProductDetail";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutForm />} />
+        <Route path="/" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
 
