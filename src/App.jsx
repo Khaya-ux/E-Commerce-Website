418702bdfe074c70d9c1befa52eb1e896282da0c
StellaNDL <157975414+StellaNDL@users.noreply.github.com>
2025-11-17 19:30:51 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,4 @@
-import React from "react";
 
-import "./index.css";
-import Sidebar from "./components/Sidebar";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="flex">
-        <Sidebar />
-
-        {/* Main page content */}
-        <div className="flex-1 p-10">
-          <Routes>
-            <Route path="/" element={<h1>Store Page</h1>} />
-            <Route path="/favorite" element={<h1>Favorite Page</h1>} />
-            <Route path="/bag" element={<h1>Bag Page</h1>} />
-            <Route path="/logout" element={<h1>Logging Out...</h1>} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
-}
-
-export default App;
 
 
