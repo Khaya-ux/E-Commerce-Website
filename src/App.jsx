33955f63b5921614c3ee6db96d8a48d42362ff6b
Khaya-ux <kdube2912@gmail.com>
2025-11-17 15:14:27 +0200

    Brendons imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CartPage from "./components/CartPage";
 import CheckoutForm from "./components/CheckoutForm";
+import ProductList from "./components/ProductList";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Route path="/" element={<div>Home Page</div>} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/checkout" element={<CheckoutForm />} />
+        <Route path="/" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   );
